2c7b910c4e875f7087ec2d14a67a90b5a5e534f8
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-09 11:02:15 +0700

    them bt

--- Rectangle/src/Rectangle.java
+++ Rectangle/src/Rectangle.java
@@ -5,6 +5,22 @@ public class Rectangle {
 
     }
 
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
     public Rectangle(double width, double height) {
         this.width = width;
         this.height = height;
